670689afb5c1a8658973dd5739100f70d79a0e32
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2020-03-10 21:48:00 +0200

    FixOrderProducts

---
+++ FoodForYou/Data/FoodForYou.Data/Migrations/20200310193921_RenameOrderProducts.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodForYou.Data.Migrations
+{
+    public partial class RenameOrderProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProduct_Products_ProductId",
+                table: "OrderProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderProduct",
+                table: "OrderProduct");
+
+            migrationBuilder.RenameTable(
+                name: "OrderProduct",
+                newName: "OrderProducts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_ProductId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_OrderId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProduct_IsDeleted",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_IsDeleted");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderProducts",
+                table: "OrderProducts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrderId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderProducts",
+                table: "OrderProducts");
+
+            migrationBuilder.RenameTable(
+                name: "OrderProducts",
+                newName: "OrderProduct");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_ProductId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_OrderId",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_IsDeleted",
+                table: "OrderProduct",
+                newName: "IX_OrderProduct_IsDeleted");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderProduct",
+                table: "OrderProduct",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Orders_OrderId",
+                table: "OrderProduct",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProduct_Products_ProductId",
+                table: "OrderProduct",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
