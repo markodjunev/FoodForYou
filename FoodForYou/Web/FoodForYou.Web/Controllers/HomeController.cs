78ff55f5c5ff3d452273db05928827891cad38e3
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2020-03-15 15:04:16 +0200

    LoginAndHomeViews

--- FoodForYou/Web/FoodForYou.Web/Controllers/HomeController.cs
+++ FoodForYou/Web/FoodForYou.Web/Controllers/HomeController.cs
@@ -13,6 +13,11 @@
             return this.View();
         }
 
+        public IActionResult About()
+        {
+            return this.View();
+        }
+
         public IActionResult Privacy()
         {
             return this.View();
