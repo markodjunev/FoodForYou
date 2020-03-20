7034320143d4170252ed989d2fddd93f9e6826d3
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2020-03-20 11:20:33 +0200

    ChangeCategoriesImages

--- FoodForYou/Data/FoodForYou.Data/Seeding/CategoriesSeeder.cs
+++ FoodForYou/Data/FoodForYou.Data/Seeding/CategoriesSeeder.cs
@@ -20,25 +20,25 @@
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Beverages",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/beverages_k8wmhz.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695419/FoodForYou/beverages_dsviye.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Salads",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538413/FoodForYou/salads_g85ypa.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/salads_ydjdzp.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Soups",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/soups_azypsi.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695419/FoodForYou/soups_tc7nfu.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Pizza",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538413/FoodForYou/pizzas_jfag31.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/pizzas_ilrdd2.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
@@ -50,31 +50,31 @@
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Burgers",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/burgers_w0aany.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695434/FoodForYou/burgers_ha6dlp.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Seafood",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538413/FoodForYou/seafood_goo2v4.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/seafood_zcgnmr.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Meat",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/meat_loc9rq.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/meat_uj1o9l.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Desserts",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/Desserts_mavelg.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/Desserts_t6546o.jpg",
             });
 
             await dbContext.Categories.AddAsync(new Category
             {
                 Name = "Wine",
-                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584538400/FoodForYou/wine_aa5fbn.jpg",
+                ImageUrl = "https://res.cloudinary.com/dzheivzqv/image/upload/v1584695428/FoodForYou/wine_parsql.jpg",
             });
         }
     }
